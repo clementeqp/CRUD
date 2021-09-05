03e1fe8aa7709a1b8075bad0e2108564d99412db
Clementeqp <clementeqp@hotmail.com>
2021-09-05 09:25:22 +0200

    Subir proyecto

---
+++ docs/precache-manifest.ac09168b5a029b2cf8a6bbd68de37e61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c396a60a36bba20f87f62aa4c3782e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841a8529592369389d5b",
+    "url": "/static/css/2.9d71b1d6.chunk.css"
+  },
+  {
+    "revision": "9f4251f354b6083e7b78",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "841a8529592369389d5b",
+    "url": "/static/js/2.c8f20e8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c8f20e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4251f354b6083e7b78",
+    "url": "/static/js/main.ec066de8.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
